1b945c530e981afddd70c759d8e6ba3db86344ff
Ole Felber <45100017+okiwi6@users.noreply.github.com>
2023-11-25 16:16:28 +0100

    log to wandb

--- src/train/train.py
+++ src/train/train.py
@@ -1,18 +1,16 @@
 from lightning import Trainer
 from lightning.pytorch.callbacks import StochasticWeightAveraging, LearningRateMonitor, ModelCheckpoint
-from lightning.pytorch.tuner import Tuner
-from ..models.ssdnet import SSDNet
+from lightning.pytorch.loggers import WandbLogger
+
 from ..models.lightning import LightningWrapper
 from ..dataloader.lightningdataset import DataModule
-from ..dataloader.torchdataset import BBoxDataset
-from ..augmentation.augmenter import Augmenter
 import torch
 
 
 def main():
-    BATCH_SIZE = 32
+    BATCH_SIZE = 64
     # image_size = (480, 640)
-    image_size = (480 // 2, 640 // 2)
+    image_size = (480 // 4, 640 // 4)
     num_classes = 1 + 4
     model = LightningWrapper(
         image_size,
@@ -22,16 +20,19 @@ def main():
         iou_threshold=0.5,
         conf_threshold=0.2,
         detections_per_img=50,
-        learning_rate_reduction_factor=0.8
+        learning_rate_reduction_factor=0.8,
+        out_channels=256,
     )
 
     dataloader = DataModule(image_size, num_workers=28, batch_size=BATCH_SIZE)
 
+    logger = WandbLogger(project="detection")
     trainer = Trainer(
-        max_epochs=800,
+        logger=logger,
+        max_epochs=400,
         callbacks=[
             ModelCheckpoint(save_top_k=1, monitor="val/iou", mode="max"),
-            StochasticWeightAveraging(swa_lrs=1e-2),
+            # StochasticWeightAveraging(swa_lrs=1e-2),
             LearningRateMonitor(logging_interval='step')
         ],
         fast_dev_run=False)
