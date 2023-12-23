ba030ffbbf045080a5b57b05cebfa7c928546e9d
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-12-23 23:52:04 +0100

    Update model configuration and training parameters

--- src/train/train.py
+++ src/train/train.py
@@ -9,49 +9,45 @@ from tqdm import tqdm
 
 def main():
     BATCH_SIZE = 256
-    # image_size = (480, 640)
     image_size = (480 // 4, 640 // 4)
     num_classes = 1 + 4
+
+    # example models:
+    # - 'repvit_m0_9'
+    # - 'mobilenetv3_small_050', 'mobilenetv3_small_075', 'mobilenetv3_small_100'
+    # - 'mobileone_s0', 'mobileone_s1'
+    # - 'efficientnet_b0', 'efficientnet_b1', 'efficientnet_b2', 'efficientnetv2_s'
+
     model = LightningWrapper(
         image_size,
         num_classes,
-        model="mobilenetv3",
+        model="repvit_m0_9",
         batch_size=BATCH_SIZE,
         iou_threshold=0.5,
         conf_threshold=0.2,
         detections_per_img=50,
         learning_rate_reduction_factor=0.8,
-        out_channels=128,
+        out_channels=32,
         initial_learning_rate=2e-3,
+        use_fpn=True,
         pretrained_weights=False,
     )
 
-    dataloader = DataModule(image_size, num_workers=12, batch_size=BATCH_SIZE)
-    dataloader.setup("fit")
-    class_count = torch.zeros(num_classes, dtype=torch.long)
-
-    # for (images, labels) in tqdm(dataloader.train_dataloader()):
-    #     for label in labels:
-    #         class_count[label["labels"]] += 1
-
-    print(f"class count for objects: {list(class_count)}")
+    dataloader = DataModule(image_size, num_workers=8, batch_size=BATCH_SIZE)
 
     logger = WandbLogger(project="detection")
     trainer = Trainer(
         logger=logger,
         gradient_clip_val=100.0,
         max_epochs=800,
-        # precision="16-mixed",
+        precision="16-mixed",
+        # strategy="deepspeed_stage_2",
         callbacks=[
-            ModelCheckpoint(save_top_k=1, monitor="val/map", mode="max"),
-            StochasticWeightAveraging(swa_lrs=1e-4, annealing_epochs=50),
+            ModelCheckpoint(save_top_k=1, monitor="val/ma_f1", mode="max"),
+            # StochasticWeightAveraging(swa_lrs=1e-4, annealing_epochs=50),
             LearningRateMonitor(logging_interval='step')
         ],
         fast_dev_run=False)
-
-    # Does not work in DDP
-    # tuner = Tuner(trainer)
-    # tuner.scale_batch_size(model, "binsearch")
 
     trainer.fit(model, dataloader)
 
